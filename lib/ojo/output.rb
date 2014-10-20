4b512cbd606cc51cf960b6b49ec4f23d4a48762b
geordie <george.speake@gmail.com>
2014-10-20 09:32:11 -0700

    refactoring output methods to improve codeclimate grade.

--- lib/ojo/output.rb
+++ lib/ojo/output.rb
@@ -1,47 +1,60 @@
 module Ojo
   def self.display_to_console(data)
-    Table.header("Ojo v.#{VERSION}")
-    Table.header("file location: #{data[:location]}")
-    Table.header(Date.today.strftime('%m/%d/%Y'))
+    format_table data
 
-    Table.column(data[:branch_1], :width => 60, :padding => 2, :justification => :left)
-    Table.column(data[:branch_2], :width => 60, :padding => 2, :justification => :left)
-    Table.column('Results', :width => 11, :justification => :center)
-
-    all_same = true
     failure_count = 0
 
     data[:results].each_key do |basename|
-      file_1 = data[:results][basename][:file_1]
-      file_1 = File.basename(file_1) if file_1
+      file_1 = file_basename(data[:results][basename][:file_1])
+      file_2 = file_basename(data[:results][basename][:file_2])
 
-      file_2 = data[:results][basename][:file_2]
-      file_2 = File.basename(file_2) if file_2
-
-      same = data[:results][basename][:same]
-
-      color = :blue
+      color       = :blue
       result_text = '--'
-      if same
-        color = :green
-        result_text = 'PASS'
-      elsif same == false      # need to check for false as nil is possible, but not the same as false. TODO: CHANGE THIS. FEELS YUCKY.
-        color = :red
-        result_text = 'FAIL'
-        all_same = false
-        failure_count += 1
+
+      if test_performed?(data[:results][basename])
+        same = data[:results][basename][:same]
+        color = same ? :green : :red
+        result_text = same ? 'PASS' : 'FAIL'
+        failure_count += 1 unless same
       end
 
       one_row file_1, file_2, result_text, color
     end
+    format_table_footer failure_count, data
+  end
 
-    Table.footer(results_message(all_same, failure_count), :justification => :center)
+  private
+
+  def self.format_table_footer(failure_count, data)
+    Table.footer(results_message(failure_count == 0, failure_count), :justification => :center)
     Table.footer("Difference Files at #{File.join(data[:location], 'diff')}", :justification => :center)
 
     Table.tabulate
   end
 
-  private
+  def self.file_basename(filename)
+    out = filename
+    out = File.basename(filename) unless out.nil?
+    out
+  end
+
+  def self.test_performed?(data)
+    !data[:same].nil?
+  end
+
+  def self.format_table_header(data)
+    Table.header("Ojo v.#{VERSION}")
+    Table.header("file location: #{data[:location]}")
+    Table.header(Date.today.strftime('%m/%d/%Y'))
+  end
+
+  def self.format_table(data)
+    format_table_header data
+
+    Table.column(data[:branch_1], :width => 60, :padding => 2, :justification => :left)
+    Table.column(data[:branch_2], :width => 60, :padding => 2, :justification => :left)
+    Table.column('Results', :width => 11, :justification => :center)
+  end
 
   def self.one_row(file_1, file_2, result_text, color)
     max_printable_length = 50
@@ -61,16 +74,15 @@ module Ojo
   end
 
   def self.make_printable_name(input, max_length)
-    output = input
+    output = input || ''
 
-    if input && (output.length > max_length)
-      spacer = '....'
+    if output.length > max_length
       how_much_too_long = output.length - max_length
       center_of_string = output.length / 2
       front_of_new_string = output[0..(center_of_string - how_much_too_long/2 - 3)]
       back_of_new_string = output[(center_of_string + how_much_too_long/2 + 3)..output.length]
 
-      output = front_of_new_string + spacer + back_of_new_string
+      output = "#{front_of_new_string}....#{back_of_new_string}"
     end
 
     output
